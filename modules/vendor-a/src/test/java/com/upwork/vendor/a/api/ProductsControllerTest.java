1e9ce2f8d2b82a4040e814bcbf2556df75aced20
Alexander Vivas <falexvr@gmail.com>
2025-09-28 15:04:14 +0200

    Added tests for the vendar a controller

---
+++ modules/vendor-a/src/test/java/com/upwork/vendor/a/api/ProductsControllerTest.java
@@ -0,0 +1,32 @@
+package com.upwork.vendor.a.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(ProductsController.class)
+class ProductsControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void returnsSampleProducts() throws Exception {
+        mockMvc.perform(get("/products"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(2)))
+                .andExpect(jsonPath("$[0].sku", is("ABC123")))
+                .andExpect(jsonPath("$[0].stockQuantity", is(8)))
+                .andExpect(jsonPath("$[1].sku", is("LMN789")))
+                .andExpect(jsonPath("$[1].stockQuantity", is(0)));
+
+    }
+
+}
