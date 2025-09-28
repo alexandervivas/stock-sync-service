ea25ad3d87feb6dd6fe4231563cb60dabf47b331
Alexander Vivas <falexvr@gmail.com>
2025-09-28 14:50:58 +0200

    Added libraries and yml configuration for vendor a

--- modules/vendor-a/build.gradle.kts
+++ modules/vendor-a/build.gradle.kts
@@ -1,7 +1,17 @@
 plugins {
     java
+    id("org.springframework.boot")
+    id("io.spring.dependency-management")
 }
 
 group = "com.upwork.stock"
 version = "0.0.1-SNAPSHOT"
-description = "Vendor A (mock API) - Iter 0"
+description = "Vendor A (mock API)"
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.assertj:assertj-core")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
