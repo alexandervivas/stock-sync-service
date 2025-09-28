ce5d8fa752a680861f1a4a18f5de6451cd76e846
Alexander Vivas <falexvr@gmail.com>
2025-09-28 23:36:55 +0200

    Added rabbit config for spring

---
+++ modules/stock-service/src/main/java/com/upwork/stock/config/RabbitConfig.java
@@ -0,0 +1,30 @@
+package com.upwork.stock.config;
+
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitConfig {
+    @Bean
+    public TopicExchange stockEventsExchange(@Value("${events.broker.exchange}") String name) {
+        return new TopicExchange(name, true, false);
+    }
+
+    @Bean
+    public Jackson2JsonMessageConverter jacksonConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory, Jackson2JsonMessageConverter jsonMessageConverter) {
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(jsonMessageConverter);
+
+        return rabbitTemplate;
+    }
+}
