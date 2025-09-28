b177d5e54037f6cedf915cd1182d1c221d042ce7
Alexander Vivas <falexvr@gmail.com>
2025-09-28 14:07:12 +0200

    Added spring boot library for exposing rest services

--- modules/stock-service/build.gradle.kts
+++ modules/stock-service/build.gradle.kts
@@ -9,13 +9,21 @@ version = "0.0.1-SNAPSHOT"
 description = "Stock Sync Service - Dev case"
 
 dependencies {
-    compileOnly("org.projectlombok:lombok")
-    annotationProcessor("org.projectlombok:lombok")
-
+    // spring-boot
     implementation("org.springframework.boot:spring-boot-starter")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+
+    // h2
     runtimeOnly("com.h2database:h2")
 
+    // lombok
+    compileOnly("org.projectlombok:lombok")
+    annotationProcessor("org.projectlombok:lombok")
+    testCompileOnly("org.projectlombok:lombok")
+    testAnnotationProcessor("org.projectlombok:lombok")
+
+    // tests
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.assertj:assertj-core")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
