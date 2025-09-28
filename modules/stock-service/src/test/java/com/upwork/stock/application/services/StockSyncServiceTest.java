810b477f975303a6eab49aa13b6bb70430fa299e
Alexander Vivas <falexvr@gmail.com>
2025-09-28 21:05:21 +0200

    Added tests

---
+++ modules/stock-service/src/test/java/com/upwork/stock/application/services/StockSyncServiceTest.java
@@ -0,0 +1,54 @@
+package com.upwork.stock.application.services;
+
+import com.upwork.stock.application.dto.ExternalProductDto;
+import com.upwork.stock.application.ports.VendorAClient;
+import com.upwork.stock.application.ports.VendorBReader;
+import com.upwork.stock.domain.product.Product;
+import com.upwork.stock.domain.product.ProductRepository;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.*;
+
+class StockSyncServiceTest {
+
+    @Test
+    void testSyncStock() {
+        // arrange
+        VendorAClient vendorAClient = mock(VendorAClient.class);
+        VendorBReader vendorBReader = mock(VendorBReader.class);
+        ProductRepository productRepository = mock(ProductRepository.class);
+
+        when(vendorAClient.fetchProducts()).thenReturn(List.of(
+                new ExternalProductDto("ABC123", "Product A", 8)
+        ));
+        when(vendorBReader.readProducts()).thenReturn(List.of(
+                new ExternalProductDto("XYZ456", "Product B", 0)
+        ));
+
+        StockSyncService stockSyncService = new StockSyncService(vendorAClient, vendorBReader, productRepository);
+
+        // act
+        stockSyncService.syncOnce();
+
+        // assert
+        ArgumentCaptor<Product> captor = ArgumentCaptor.forClass(Product.class);
+        verify(productRepository, atLeastOnce()).save(captor.capture());
+
+        List<Product> savedProducts = captor.getAllValues();
+        assertThat(savedProducts.size()).isEqualTo(2);
+        assertThat(savedProducts).extracting(Product::getSku).containsExactly("ABC123", "XYZ456");
+        assertThat(
+                savedProducts
+                        .stream()
+                        .filter(product -> product.getSku().equalsIgnoreCase("XYZ456"))
+                        .findFirst()
+                        .get()
+                        .getStockQuantity()
+        ).isEqualTo(0);
+    }
+
+}
---
+++ modules/stock-service/src/test/java/com/upwork/stock/infrastructure/fs/CsvVendorBReaderTest.java
@@ -0,0 +1,45 @@
+package com.upwork.stock.infrastructure.fs;
+
+import com.upwork.stock.application.dto.ExternalProductDto;
+import com.upwork.stock.config.StockIngestionProperties;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class CsvVendorBReaderTest {
+
+    @TempDir
+    Path tempDir;
+
+    @Test
+    void readCsvFile() throws Exception {
+        // arrange
+        Path file = tempDir.resolve("stock.csv");
+        Files.writeString(file, """
+                                sku,name,stockQuantity
+                                ABC123,Product A,10
+                                XYZ456,Product B,0
+                """);
+
+        StockIngestionProperties stockIngestionProperties = new StockIngestionProperties(
+                new StockIngestionProperties.VendorA("http://localhost:8081"),
+                new StockIngestionProperties.VendorB(file.toString())
+        );
+
+        CsvVendorBReader csvVendorBReader = new CsvVendorBReader(stockIngestionProperties);
+
+        // act
+        List<ExternalProductDto> list = csvVendorBReader.readProducts();
+
+        // assert
+        assertThat(list).hasSize(2);
+        assertThat(list.getFirst().sku()).isEqualTo("ABC123");
+        assertThat(list.get(1).stockQuantity()).isZero();
+    }
+
+}
