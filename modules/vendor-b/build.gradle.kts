7c9efaca34370e812fe3de86ffec477add067ae4
Alexander Vivas <falexvr@gmail.com>
2025-09-28 15:07:42 +0200

    Added libraries for vendor b

--- modules/vendor-b/build.gradle.kts
+++ modules/vendor-b/build.gradle.kts
@@ -1,7 +1,17 @@
 plugins {
     java
+    id("org.springframework.boot")
+    id("io.spring.dependency-management")
 }
 
 group = "com.upwork.stock"
 version = "0.0.1-SNAPSHOT"
-description = "Vendor B (CSV writer) - Iter 0"
+description = "Vendor B - CSV writer"
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.assertj:assertj-core")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
