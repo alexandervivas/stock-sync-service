642a1557ae5897eea589f8dcf50b2a986f98682c
Alexander Vivas <falexvr@gmail.com>
2025-09-28 14:31:37 +0200

    Added test for the query handler

---
+++ modules/stock-service/src/test/java/com/upwork/stock/application/queries/ListProductsQueryHandlerTest.java
@@ -0,0 +1,36 @@
+package com.upwork.stock.application.queries;
+
+import com.upwork.stock.application.view.ProductView;
+import com.upwork.stock.domain.product.Product;
+import com.upwork.stock.domain.product.ProductRepository;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.util.List;
+
+import static org.mockito.Mockito.when;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ListProductsQueryHandlerTest {
+
+    @Test
+    public void returnsViewsMappedFromDomain() {
+        // arrange
+        ProductRepository productRepository = Mockito.mock(ProductRepository.class);
+        ListProductsQueryHandler handler = new ListProductsQueryHandler(productRepository);
+
+        when(productRepository.findAll()).thenReturn(List.of(
+                new Product("ABC", "A", 10, "VendorA"),
+                new Product("XYZ", "B",  0, "VendorB")
+        ));
+
+        // act
+        List<ProductView> products = handler.handle(new ListProductsQuery());
+
+        // assert
+        assertThat(products).hasSize(2);
+        assertThat(products.getFirst().sku()).isEqualTo("ABC");
+        assertThat(products.get(1).stockQuantity()).isZero();
+    }
+
+}
