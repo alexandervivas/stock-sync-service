e2e5d1308324cbb7d9e8539ef0b15bc9508089b8
Alexander Vivas <falexvr@gmail.com>
2025-09-28 12:53:01 +0200

    Added jpa annotations

--- modules/stock-service/src/main/java/com/upwork/stock/domain/product/Product.java
+++ modules/stock-service/src/main/java/com/upwork/stock/domain/product/Product.java
@@ -1,11 +1,29 @@
 package com.upwork.stock.domain.product;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "products",
+        uniqueConstraints = @UniqueConstraint(
+                name = "uk_product_sku_vendor",
+                columnNames = {"sku", "vendor"}
+        )
+)
 public class Product {
 
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false, length = 128)
     private String sku;
+
+    @Column(nullable = false, length = 256)
     private String name;
+
+    @Column(nullable = false)
     private Integer stockQuantity;
+
+    @Column(nullable = false, length = 64)
     private String vendor;
 
     public Product(String sku, String name, Integer stockQuantity, String vendor) {
@@ -15,4 +33,27 @@ public class Product {
         this.vendor = vendor;
     }
 
+    protected Product() {
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getSku() {
+        return sku;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getStockQuantity() {
+        return stockQuantity;
+    }
+
+    public String getVendor() {
+        return vendor;
+    }
 }
