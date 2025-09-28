2e3779f1f9307c620eee3c6cccbe2a94b616f00c
Alexander Vivas <falexvr@gmail.com>
2025-09-28 15:18:48 +0200

    Added startup class for vendor b

---
+++ modules/vendor-b/src/main/java/com/upwork/vendor/b/VendorBApplication.java
@@ -0,0 +1,11 @@
+package com.upwork.vendor.b;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VendorBApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(VendorBApplication.class, args);
+    }
+}
