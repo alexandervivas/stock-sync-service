db9cf468e588121ba04bed6e0551273997d7d342
Alexander Vivas <falexvr@gmail.com>
2025-09-28 21:13:05 +0200

    Added a new config to handle preiodical lookups to both vendors

--- modules/stock-service/src/main/java/com/upwork/stock/config/Config.java
+++ modules/stock-service/src/main/java/com/upwork/stock/config/Config.java
@@ -3,9 +3,11 @@ package com.upwork.stock.config;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.retry.annotation.EnableRetry;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @Configuration
 @EnableRetry
+@EnableScheduling
 @EnableConfigurationProperties(StockIngestionProperties.class)
 public class Config {
 
--- modules/stock-service/src/main/java/com/upwork/stock/config/StockIngestionProperties.java
+++ modules/stock-service/src/main/java/com/upwork/stock/config/StockIngestionProperties.java
@@ -3,13 +3,17 @@ package com.upwork.stock.config;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties(prefix = "ingestion")
-public record StockIngestionProperties(VendorA vendorA, VendorB vendorB) {
+public record StockIngestionProperties(VendorA vendorA, VendorB vendorB, Sync sync) {
 
     public record VendorA(String baseUrl) {
 
     }
 
     public record VendorB(String csvPath) {
+
+    }
+
+    public record Sync(String cron) {
 
     }
 }
